3b20b8203b7922cc50757789b25dcfddeb55c7ca
aindriu80 <aindriu80@gmail.com>
2021-03-22 16:39:39 +0000

    Setting up page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Slider from "./Slider";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="main-image" />
+      <div className="sidebar">Sidebar</div>
+      <Slider />
+    </div>
+  );
+}
+
+export default App;
